87c346dff14b04baa5af6e033d90bb55c201a375
Kelly Yong <kelly.a.yong@gmail.com>
2021-03-11 18:19:03 -0500

    Use ternary operator to shorten code

--- substrings.rb
+++ substrings.rb
@@ -2,11 +2,7 @@ def substrings(string, dictionary)
     new_string = string.downcase
     result = Hash.new(0) 
     dictionary.map do |word| 
-        new_string.scan(word) do
-            if result.has_key?(word) then result[word] += 1      
-            else result[word] = 1
-            end   
-        end
+        new_string.scan(word) { result.has_key?(word)? result[word] += 1 : result[word] = 1 }
     end
     puts result
 end
